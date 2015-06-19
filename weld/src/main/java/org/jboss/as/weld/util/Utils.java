f81b076500e900f46f2f3ddab2af70d726cf7a08
Jozef Hartinger <jharting@redhat.com>
2015-06-19 09:13:42 +0200

    WFLY-3334 Event with qualifier @Initialized not raised if no webapp is deployed

--- weld/src/main/java/org/jboss/as/weld/util/Utils.java
+++ weld/src/main/java/org/jboss/as/weld/util/Utils.java
@@ -16,6 +16,8 @@
  */
 package org.jboss.as.weld.util;
 
+import java.util.Map;
+
 import org.jboss.as.ee.component.ComponentDescription;
 import org.jboss.as.ee.component.EEApplicationClasses;
 import org.jboss.as.ee.component.EEModuleDescription;
@@ -67,5 +69,11 @@ public class Utils {
         final WebComponentDescription componentDescription = new WebComponentDescription(listener, listener, module, deploymentUnit.getServiceName(), applicationClasses);
         module.addComponent(componentDescription);
         deploymentUnit.addToAttachmentList(WebComponentDescription.WEB_COMPONENTS, componentDescription.getStartServiceName());
+    }
+
+    public static <K, V> void putIfValueNotNull(Map<K, V> map, K key, V value) {
+        if (value != null) {
+            map.put(key, value);
+        }
     }
 }
