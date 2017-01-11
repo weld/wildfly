623d50403d1ba5cc978517818abe829c351d1265
Matej Novotny <manovotn@redhat.com>
2017-01-11 14:04:32 +0100

    Align with core/pull/1558.

--- weld/src/main/java/org/jboss/as/weld/ejb/EjbRequestScopeActivationInterceptor.java
+++ weld/src/main/java/org/jboss/as/weld/ejb/EjbRequestScopeActivationInterceptor.java
@@ -29,8 +29,8 @@ import org.jboss.invocation.InterceptorFactoryContext;
 import org.jboss.msc.service.ServiceContainer;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.weld.context.ejb.EjbRequestContext;
-import org.jboss.weld.ejb.AbstractEJBRequestScopeActivationInterceptor;
 import org.jboss.weld.manager.BeanManagerImpl;
+import org.jboss.weld.module.ejb.AbstractEJBRequestScopeActivationInterceptor;
 
 /**
  * Interceptor for activating the CDI request scope on some EJB invocations.
